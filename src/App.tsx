76e2bdf2b3a0cbfca9127d92a06e20af5b51f4ef
Fernandasacosta <fernandasacostaa@gmail.com>
2024-12-10 14:50:00 -0300

    Estrutura da Aplicação - Configurando temas

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
+import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button";
+import { defaultTime } from "./styles/themes/default";
 
 export function App() {
 
   return (
-    <>
+    <ThemeProvider theme={defaultTime}>
     <Button variant="primary" />
     <Button variant="secondary" />
     <Button variant="success" />
     <Button variant="danger" />
     <Button />
-  </>
+  </ThemeProvider>
   )
 }
 
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,4 @@
+export const defaultTime = {
+  primary: 'grey',
+  secondary: 'green'
+}
